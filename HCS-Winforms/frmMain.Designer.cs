33fa1687cd4212367def8e0349972c0e4ab11de0
Daniel Schiffer <daniel.schiffer@hotmail.de>
2024-03-24 19:03:13 +0100

    Weitere Anpassungen UI

--- HCS-Winforms/frmMain.Designer.cs
+++ HCS-Winforms/frmMain.Designer.cs
@@ -32,13 +32,24 @@
             MainMenu = new MenuStrip();
             testfensterToolStripMenuItem = new ToolStripMenuItem();
             flightToolStripMenuItem = new ToolStripMenuItem();
+            simbriefToolStripMenuItem = new ToolStripMenuItem();
+            navDataToolStripMenuItem = new ToolStripMenuItem();
             tableLayoutPanel1 = new TableLayoutPanel();
-            gbStammdaten = new GroupBox();
             groupBox1 = new GroupBox();
+            label9 = new Label();
+            label8 = new Label();
+            btnDestSugestion = new Button();
             maskedTextBox2 = new MaskedTextBox();
             maskedTextBox1 = new MaskedTextBox();
             label4 = new Label();
+            gbStammdaten = new GroupBox();
             gbHeute = new GroupBox();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            cbFlightNumberLock = new CheckBox();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
             tbUTC = new TextBox();
             tbLocalTime = new TextBox();
             tbDatum = new TextBox();
@@ -49,19 +60,19 @@
             imageList1 = new ImageList(components);
             MainMenu.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
-            gbStammdaten.SuspendLayout();
             groupBox1.SuspendLayout();
+            gbStammdaten.SuspendLayout();
             gbHeute.SuspendLayout();
             SuspendLayout();
             // 
             // MainMenu
             // 
             MainMenu.ImageScalingSize = new Size(24, 24);
-            MainMenu.Items.AddRange(new ToolStripItem[] { testfensterToolStripMenuItem, flightToolStripMenuItem });
+            MainMenu.Items.AddRange(new ToolStripItem[] { testfensterToolStripMenuItem, flightToolStripMenuItem, simbriefToolStripMenuItem, navDataToolStripMenuItem });
             MainMenu.Location = new Point(0, 0);
             MainMenu.Name = "MainMenu";
             MainMenu.Padding = new Padding(12, 4, 0, 4);
-            MainMenu.Size = new Size(1485, 27);
+            MainMenu.Size = new Size(1006, 27);
             MainMenu.TabIndex = 0;
             MainMenu.Text = "menuStrip1";
             // 
@@ -78,79 +89,130 @@
             flightToolStripMenuItem.Size = new Size(49, 19);
             flightToolStripMenuItem.Text = "Flight";
             // 
+            // simbriefToolStripMenuItem
+            // 
+            simbriefToolStripMenuItem.Name = "simbriefToolStripMenuItem";
+            simbriefToolStripMenuItem.Size = new Size(63, 19);
+            simbriefToolStripMenuItem.Text = "Simbrief";
+            // 
+            // navDataToolStripMenuItem
+            // 
+            navDataToolStripMenuItem.Name = "navDataToolStripMenuItem";
+            navDataToolStripMenuItem.Size = new Size(69, 19);
+            navDataToolStripMenuItem.Text = "Nav-Data";
+            // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 3;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(groupBox1, 0, 1);
             tableLayoutPanel1.Controls.Add(gbStammdaten, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 27);
             tableLayoutPanel1.Margin = new Padding(5, 6, 5, 6);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 3;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(1485, 877);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 42.2118378F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 23.2087231F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 34.57944F));
+            tableLayoutPanel1.Size = new Size(1006, 642);
             tableLayoutPanel1.TabIndex = 1;
-            // 
-            // gbStammdaten
-            // 
-            gbStammdaten.Controls.Add(groupBox1);
-            gbStammdaten.Controls.Add(gbHeute);
-            gbStammdaten.Dock = DockStyle.Fill;
-            gbStammdaten.Location = new Point(4, 4);
-            gbStammdaten.Margin = new Padding(4);
-            gbStammdaten.Name = "gbStammdaten";
-            gbStammdaten.Padding = new Padding(4);
-            gbStammdaten.Size = new Size(487, 284);
-            gbStammdaten.TabIndex = 0;
-            gbStammdaten.TabStop = false;
-            gbStammdaten.Text = "Stammdaten";
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(btnDestSugestion);
             groupBox1.Controls.Add(maskedTextBox2);
             groupBox1.Controls.Add(maskedTextBox1);
             groupBox1.Controls.Add(label4);
-            groupBox1.Location = new Point(12, 183);
+            groupBox1.Location = new Point(3, 274);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(290, 71);
+            groupBox1.Size = new Size(312, 89);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Route";
             // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(132, 21);
+            label9.Margin = new Padding(4, 0, 4, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(89, 21);
+            label9.TabIndex = 7;
+            label9.Text = "Destination";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(4, 21);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(74, 21);
+            label8.TabIndex = 7;
+            label8.Text = "Depature";
+            // 
+            // btnDestSugestion
+            // 
+            btnDestSugestion.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDestSugestion.Location = new Point(231, 33);
+            btnDestSugestion.Name = "btnDestSugestion";
+            btnDestSugestion.Size = new Size(75, 46);
+            btnDestSugestion.TabIndex = 5;
+            btnDestSugestion.Text = "Destination\r\nSuggestion";
+            btnDestSugestion.UseVisualStyleBackColor = true;
+            // 
             // maskedTextBox2
             // 
-            maskedTextBox2.Location = new Point(134, 28);
+            maskedTextBox2.Location = new Point(132, 45);
             maskedTextBox2.Mask = "LLLL";
             maskedTextBox2.Name = "maskedTextBox2";
-            maskedTextBox2.Size = new Size(75, 29);
+            maskedTextBox2.Size = new Size(89, 29);
             maskedTextBox2.TabIndex = 4;
             maskedTextBox2.TextAlign = HorizontalAlignment.Center;
             // 
             // maskedTextBox1
             // 
-            maskedTextBox1.Location = new Point(8, 28);
+            maskedTextBox1.Location = new Point(9, 45);
             maskedTextBox1.Mask = "LLLL";
             maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(75, 29);
+            maskedTextBox1.Size = new Size(89, 29);
             maskedTextBox1.TabIndex = 3;
             maskedTextBox1.TextAlign = HorizontalAlignment.Center;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(89, 31);
+            label4.Location = new Point(99, 48);
             label4.Name = "label4";
-            label4.Size = new Size(39, 21);
+            label4.Size = new Size(33, 21);
             label4.TabIndex = 2;
-            label4.Text = "--->";
+            label4.Text = "-->";
+            // 
+            // gbStammdaten
+            // 
+            gbStammdaten.Controls.Add(gbHeute);
+            gbStammdaten.Dock = DockStyle.Fill;
+            gbStammdaten.Location = new Point(4, 4);
+            gbStammdaten.Margin = new Padding(4);
+            gbStammdaten.Name = "gbStammdaten";
+            gbStammdaten.Padding = new Padding(4);
+            gbStammdaten.Size = new Size(327, 263);
+            gbStammdaten.TabIndex = 0;
+            gbStammdaten.TabStop = false;
+            gbStammdaten.Text = "Stammdaten";
             // 
             // gbHeute
             // 
+            gbHeute.Controls.Add(textBox1);
+            gbHeute.Controls.Add(label7);
+            gbHeute.Controls.Add(cbFlightNumberLock);
+            gbHeute.Controls.Add(label6);
+            gbHeute.Controls.Add(comboBox1);
+            gbHeute.Controls.Add(label5);
             gbHeute.Controls.Add(tbUTC);
             gbHeute.Controls.Add(tbLocalTime);
             gbHeute.Controls.Add(tbDatum);
@@ -162,14 +224,70 @@
             gbHeute.Margin = new Padding(4);
             gbHeute.Name = "gbHeute";
             gbHeute.Padding = new Padding(4);
-            gbHeute.Size = new Size(290, 138);
+            gbHeute.Size = new Size(277, 207);
             gbHeute.TabIndex = 0;
             gbHeute.TabStop = false;
             gbHeute.Text = "Heute";
             // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(102, 119);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(113, 29);
+            textBox1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(221, 103);
+            label7.Name = "label7";
+            label7.Size = new Size(30, 13);
+            label7.TabIndex = 10;
+            label7.Text = "Lock";
+            // 
+            // cbFlightNumberLock
+            // 
+            cbFlightNumberLock.AutoSize = true;
+            cbFlightNumberLock.Location = new Point(229, 119);
+            cbFlightNumberLock.Name = "cbFlightNumberLock";
+            cbFlightNumberLock.Size = new Size(15, 14);
+            cbFlightNumberLock.TabIndex = 9;
+            cbFlightNumberLock.TextAlign = ContentAlignment.MiddleCenter;
+            cbFlightNumberLock.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(102, 95);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(113, 21);
+            label6.TabIndex = 8;
+            label6.Text = "Flight-Number";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(8, 119);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(67, 29);
+            comboBox1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(8, 95);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 21);
+            label5.TabIndex = 6;
+            label5.Text = "Airline";
+            // 
             // tbUTC
             // 
-            tbUTC.Location = new Point(186, 70);
+            tbUTC.Location = new Point(186, 51);
             tbUTC.Margin = new Padding(4);
             tbUTC.Name = "tbUTC";
             tbUTC.ReadOnly = true;
@@ -180,7 +298,7 @@
             // 
             // tbLocalTime
             // 
-            tbLocalTime.Location = new Point(102, 70);
+            tbLocalTime.Location = new Point(102, 51);
             tbLocalTime.Margin = new Padding(4);
             tbLocalTime.Name = "tbLocalTime";
             tbLocalTime.ReadOnly = true;
@@ -191,7 +309,7 @@
             // 
             // tbDatum
             // 
-            tbDatum.Location = new Point(8, 70);
+            tbDatum.Location = new Point(8, 51);
             tbDatum.Margin = new Padding(4);
             tbDatum.Name = "tbDatum";
             tbDatum.ReadOnly = true;
@@ -202,7 +320,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(186, 45);
+            label3.Location = new Point(186, 26);
             label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
             label3.Size = new Size(38, 21);
@@ -212,7 +330,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(102, 45);
+            label2.Location = new Point(102, 26);
             label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
             label2.Size = new Size(46, 21);
@@ -222,7 +340,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(8, 45);
+            label1.Location = new Point(8, 26);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
             label1.Size = new Size(57, 21);
@@ -232,10 +350,10 @@
             // StatusBar
             // 
             StatusBar.ImageScalingSize = new Size(24, 24);
-            StatusBar.Location = new Point(0, 882);
+            StatusBar.Location = new Point(0, 647);
             StatusBar.Name = "StatusBar";
             StatusBar.Padding = new Padding(1, 0, 26, 0);
-            StatusBar.Size = new Size(1485, 22);
+            StatusBar.Size = new Size(1006, 22);
             StatusBar.TabIndex = 2;
             StatusBar.Text = "statusStrip1";
             // 
@@ -249,7 +367,7 @@
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1485, 904);
+            ClientSize = new Size(1006, 669);
             Controls.Add(StatusBar);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(MainMenu);
@@ -261,9 +379,9 @@
             MainMenu.ResumeLayout(false);
             MainMenu.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
-            gbStammdaten.ResumeLayout(false);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            gbStammdaten.ResumeLayout(false);
             gbHeute.ResumeLayout(false);
             gbHeute.PerformLayout();
             ResumeLayout(false);
@@ -290,5 +408,16 @@
         private MaskedTextBox maskedTextBox1;
         private Label label4;
         private MaskedTextBox maskedTextBox2;
+        private Button btnDestSugestion;
+        private ToolStripMenuItem simbriefToolStripMenuItem;
+        private ToolStripMenuItem navDataToolStripMenuItem;
+        private Label label5;
+        private TextBox textBox1;
+        private Label label7;
+        private CheckBox cbFlightNumberLock;
+        private Label label6;
+        private ComboBox comboBox1;
+        private Label label9;
+        private Label label8;
     }
 }
